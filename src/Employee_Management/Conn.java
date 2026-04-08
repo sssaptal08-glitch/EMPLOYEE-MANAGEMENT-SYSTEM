f1a3b9fd6f4c747083da039b67cbcbf86b7650c7
Sujal Saptal <sssaptal08@gmail.com>
2026-04-08 17:12:49 +0530

    Initial commit

---
+++ src/Employee_Management/Conn.java
@@ -0,0 +1,19 @@
+package Employee_Management;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "Dhurba@69");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
